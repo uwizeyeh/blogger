cb9644ffa9cee647eb676cbd3c20d150f4fca864
Katy Aristote <stotestorch@gmail.com>
2019-02-21 11:32:09 +0200

    Changed the structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,25 @@
 from flask import Flask
-from .config import DevConfig
 from flask_bootstrap import Bootstrap
+from config import config_options
 
-# Initializing application
-app = Flask(__name__,instance_relative_config = True)
+bootstrap = Bootstrap()
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+def create_app(config_name):
 
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
+    app = Flask(__name__)
 
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
 
-from app import views
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .request import configure_request
+    configure_request(app)
+    # Will add the views and forms
+
+    return app
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY=b8e19cc3ea78b217ae0eddb1a13646d1
+export SECRET_KEY=12345
+
+python3.6 manage.py server
