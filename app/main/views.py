c99e472f7490e688093176e0b161d4ec1e76aa49
uwizeyeh <uwizeyimanahulde1@gmail.com>
2019-02-27 18:45:51 +0200

    add pitchForm

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,request,redirect,url_for,abort
 from . import main
-from .forms import ReviewForm,UpdateProfile
+from .forms import ReviewForm,UpdateProfile,PitchForm
 from ..models import User
 from flask_login import login_required
 from .. import db
